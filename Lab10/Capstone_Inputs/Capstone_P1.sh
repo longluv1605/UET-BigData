47f8ea703b9a363da58d507ebc0d6955868d20cd
longluv1605 <longtrong53@gmail.com>
2024-11-20 22:05:30 +0700

    OK capstone.sh

--- Lab10/Capstone_Inputs/Capstone_P1.sh
+++ Lab10/Capstone_Inputs/Capstone_P1.sh
@@ -5,16 +5,18 @@ find . -name "*.java" -exec rm {} \;
 rm -r /home/hadoop/Capstone_Project
 mkdir -p /home/hadoop/Capstone_Project
 
-rm -r /home/hadoop/Capstone_Project/Capstone_Outputs
+# rm -r /home/hadoop/Capstone_Project/Capstone_Outputs
 mkdir -p /home/hadoop/Capstone_Project/Capstone_Outputs
+mkdir -p /home/hadoop/Capstone_Project/Capstone_Inputs
 cp -r /home/hadoop/Documents/UET-BigData/Lab10/Capstone_Inputs/* /home/hadoop/Capstone_Project/Capstone_Inputs
+cd /home/hadoop/Capstone_Project/Capstone_Inputs
 
 mysql -u hive -p000000 -D capstone_proj -e 'source CreateMySQLTables.sql' > /home/hadoop/Capstone_Project/Capstone_Outputs/Cap_MySQLTables.txt
 
 hdfs dfs -rm -r /user/hive/warehouse/Capstone
 hdfs dfs -mkdir -p /user/hive/warehouse/Capstone
 
-sqoop import-all-tables --connect jdbc:mysql://ip-10-1-1-204.ap-south-1.compute.internal:3306/hive --username hive --password 000000 --compression-codec=snappy --as-avrodatafile --warehouse-dir=/user/hive/warehouse/Capstone --m 1 --driver com.mysql.cj.jdbc.Driver
+sqoop import-all-tables --connect jdbc:mysql://localhost:3306/capstone_proj --username hive --password 000000 --compression-codec=snappy --as-avrodatafile --warehouse-dir=/user/hive/warehouse/Capstone --m 1 --exclude-tables AUX_TABLE
 
 hdfs dfs -rm -r /user/hive/avsc
 hdfs dfs -mkdir -p /user/hive/avsc
